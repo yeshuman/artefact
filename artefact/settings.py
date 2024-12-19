96e1f5601150f269ec2e5c1180a3242fdd6fe54f
yeshuman <daryl@yeshuman.io>
2024-12-19 20:20:35 +1100

    wip

--- artefact/settings.py
+++ artefact/settings.py
@@ -92,8 +92,12 @@ ASGI_APPLICATION = 'artefact.asgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'artefact'),
+        'USER': os.getenv('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
 
